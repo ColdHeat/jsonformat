26a9da488d6f1ca72e8670627b06e551959ac403
Kevin Chung <kchung@nyu.edu>
2021-06-20 15:41:29 -0400

    Add safety check

--- format.js
+++ format.js
@@ -1,11 +1,16 @@
 (function() {
+  try {
+    var data = JSON.parse(document.body.textContent);
+  } catch (error) {
+    alert(error);
+    return;
+  }
   var css = "body { white-space: pre; font-family: monospace; }";
   var tag = document.createElement("style");
   tag.type = "text/css";
   document.head.appendChild(tag);
   tag.appendChild(document.createTextNode(css));
 
-  var data = JSON.parse(document.body.textContent);
   document.body.innerHTML = "";
   document.body.appendChild(
     document.createTextNode(JSON.stringify(data, null, 4))
